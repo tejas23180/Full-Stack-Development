708a067562b277ac9548428df269271e54066c94
tejas23180 <tejasvvp143@gmail.com>
2025-07-04 12:39:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function Submit(){
+
+    var fname=document.getElementById('fname')
+    var lname=document.getElementById('lname')
+    var fullname=fname.value+" "+lname.value
+console.log(fullname)
+    var email=document.getElementById('email')
+    var address=document.getElementById('address')
+    var full=email.value+" "+address.value
+console.log(full)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
